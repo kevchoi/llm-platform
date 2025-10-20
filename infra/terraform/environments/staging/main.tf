523701dc03fe6675c5c51ef227c24f9294e9bbcc
kev <code@kevchoi.com>
2025-10-20 16:26:37 -0400

    clean up infra

--- infra/terraform/environments/staging/main.tf
+++ infra/terraform/environments/staging/main.tf
@@ -15,40 +15,141 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "your-terraform-state-bucket"
     key     = "staging/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
   }
 }
 
-module "vpc" {
-  source = "../../modules/vpc"
-
-  name = "llm-platform"
-  env  = "staging"
+locals {
+  name         = "llm"
+  env          = "staging"
+  cluster_name = "${local.name}-${local.env}"
 }
 
-module "eks" {
-  source = "../../modules/eks"
+# VPC configuration
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.4.0"
 
-  name       = "llm-platform"
-  env        = "staging"
+  name = "${local.name}-${local.env}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+# EKS cluster configuration
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "21.4.0"
+
+  name               = local.cluster_name
+  kubernetes_version = "1.34"
+
+  endpoint_public_access                   = true
+  enable_cluster_creator_admin_permissions = true
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    main = {
+      name           = "${local.cluster_name}-main"
+      instance_types = ["t3.micro"]
+      min_size       = 2
+      max_size       = 2
+      desired_size   = 2
+    }
+  }
 }
 
-output "vpc_id" {
-  description = "The ID of the VPC."
-  value       = module.vpc.vpc_id
+# Kubernetes Provider Configuration
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
 }
 
-output "public_subnets" {
-  description = "List of IDs of public subnets."
-  value       = module.vpc.public_subnets
+# Helm Provider Configuration
+provider "helm" {
+  kubernetes = {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
+  }
 }
 
-output "private_subnets" {
-  description = "List of IDs of private subnets."
-  value       = module.vpc.private_subnets
+# ArgoCD Namespace
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+  }
 }
+
+# ArgoCD Installation
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = "9.0.3"
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+
+  depends_on = [module.eks]
+}
+
+# ArgoCD Application
+# resource "kubectl_manifest" "argocd_app" {
+#   yaml_body = {
+#     apiVersion = "argoproj.io/v1alpha1"
+#     kind       = "Application"
+#     metadata = {
+#       name      = "llm-${local.env}"
+#       namespace = "argocd"
+#     }
+#     spec = {
+#       project = "default"
+#       source = {
+#         repoURL        = "https://github.com/kevchoi/llm-platform.git"
+#         targetRevision = "HEAD"
+#         path           = "infra/k8s/${local.env}/app"
+#       }
+#       destination = {
+#         server    = "https://kubernetes.default.svc"
+#         namespace = "default"
+#       }
+#       syncPolicy = {
+#         automated = {
+#           prune    = true
+#           selfHeal = true
+#         }
+#         syncOptions = ["CreateNamespace=true"]
+#       }
+#     }
+#   }
+
+#   depends_on = [helm_release.argocd]
+# }
