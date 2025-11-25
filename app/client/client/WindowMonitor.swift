7f6e3fd62192ec9aa856ea6d7b86b04b0e03ce1f
kev <code@kevchoi.com>
2025-11-24 17:39:27 -0800

    init commit

---
+++ app/client/client/WindowMonitor.swift
@@ -0,0 +1,13 @@
+//
+//  WindowMonitor.swift
+//  client
+//
+//  Created by kev on 2025-11-24.
+//
+
+import Foundation
+import ScreenCaptureKit
+
+actor WindowMonitor {
+    private var prevWindows: [SCWindow] = []
+}
---
+++ app/client/client/clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  clientApp.swift
+//  client
+//
+//  Created by kev on 2025-11-24.
+//
+
+import SwiftUI
+
+@main
+struct clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
