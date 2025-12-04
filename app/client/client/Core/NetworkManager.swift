92195dee4453789e340139886fc1c2388ec2799d
kev <code@kevchoi.com>
2025-12-04 16:22:51 -0500

    add post

--- app/client/client/Core/NetworkManager.swift
+++ app/client/client/Core/NetworkManager.swift
@@ -1,22 +1,24 @@
 import Foundation
-import Combine
 
-// MARK: - Network Manager Protocol
-
-/// Manages communication with the remote server
 public protocol NetworkManagerProtocol: Actor {
     func upload(captureInfos: [CaptureInfo]) async throws -> UploadResponse
 }
 
-// MARK: - Network Manager Implementation
+public struct UploadResponse: Codable, Sendable {
+    public let success: Bool
+}
 
 public actor NetworkManager: NetworkManagerProtocol {
     public init() {}
     
     public func upload(captureInfos: [CaptureInfo]) async throws -> UploadResponse {
         let url = URL(string: "http://localhost:8080/upload")!
-        let request = URLRequest(url: url)
-        let (data, response) = try await URLSession.shared.data(for: request)
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("image/png", forHTTPHeaderField: "Content-Type")
+        request.httpBody = captureInfos.first?.displayInfo.screenshot
+        
+        let (data, _) = try await URLSession.shared.data(for: request)
         return try JSONDecoder().decode(UploadResponse.self, from: data)
     }
 }
